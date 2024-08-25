527835ce08e06324bbe4bc54b8351fd9f6652518
Andrew Dupont <github@andrewdupont.net>
2024-08-25 10:55:12 -0700

    Initial import

---
+++ spec/savetheclocktower-test-another-package-rename-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SavetheclocktowerTestAnotherPackageRenameView from '../lib/savetheclocktower-test-another-package-rename-view';
+
+describe('SavetheclocktowerTestAnotherPackageRenameView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
